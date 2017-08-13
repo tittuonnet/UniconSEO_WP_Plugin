e4761c204671e859f8ff26731b5e5bc1b0db6ee1
Tittu Varghese <tittuhpd@gmail.com>
2017-08-13 16:58:34 +0530

    Tab for Page SEO, Facebook OG and Twitter Card.

--- index.php
+++ index.php
@@ -26,10 +26,50 @@ function unicon_seo_content()
 function unicon_seo_html($post)
 {
   ?>
-  <div>
-    <label class="uniconLabel">Page title</label>
-  	<input class="large-text uniconInputbox" type="text" name="post_title" size="30" value="" id="title" spellcheck="true" autocomplete="off">
-  </div>
+<main class="uniconMain">
+  <input id="PageSEO" class="uniconHidden uniconTab" type="radio" name="tabs" checked>
+  <label for="PageSEO" class="uniconTab">Page SEO</label>
+
+  <input id="FacebookOG" type="radio" class="uniconHidden uniconTab" name="tabs">
+  <label for="FacebookOG" class="uniconTab">Facebook Open Graph</label>
+
+  <input id="TwitterCard" type="radio" class="uniconHidden uniconTab" name="tabs">
+  <label for="TwitterCard" class="uniconTab">Twitter Card</label>
+
+  <section id="PageSEOContent" class="unicon">
+    <div class="row uniconRow">
+      <label class="uniconLabel">Focus Keyword</label>
+    	<input class="large-text uniconInputbox" type="text" name="page_keyword" placeholder="Focus Keyword for the page" id="page_keyword" spellcheck="true" autocomplete="off">
+    </div>
+    <div class="row uniconRow">
+      <label class="uniconLabel">Page Title</label>
+    	<input class="large-text uniconInputbox" type="text" name="page_title" placeholder="Page Title" id="page_title" spellcheck="true" autocomplete="off">
+    </div>
+    <div class="row uniconRow">
+      <label class="uniconLabel">Page Description</label>
+      <textarea class="large-text uniconTextArea" placeholder="Page Description"></textarea>
+    </div>
+  </section>
+
+  <section id="FacebookOGContent" class="unicon">
+    <p>
+      Bacon ipsum dolor sit amet beef venison beef ribs kielbasa. Sausage pig leberkas, t-bone sirloin shoulder bresaola. Frankfurter rump porchetta ham. Pork belly prosciutto brisket meatloaf short ribs.
+    </p>
+    <p>
+      Brisket meatball turkey short loin boudin leberkas meatloaf chuck andouille pork loin pastrami spare ribs pancetta rump. Frankfurter corned beef beef tenderloin short loin meatloaf swine ground round venison.
+    </p>
+  </section>
+
+  <section id="TwitterCardContent" class="unicon">
+    <p>
+      Bacon ipsum dolor sit amet beef venison beef ribs kielbasa. Sausage pig leberkas, t-bone sirloin shoulder bresaola. Frankfurter rump porchetta ham. Pork belly prosciutto brisket meatloaf short ribs.
+    </p>
+    <p>
+      Brisket meatball turkey short loin boudin leberkas meatloaf chuck andouille pork loin pastrami spare ribs pancetta rump. Frankfurter corned beef beef tenderloin short loin meatloaf swine ground round venison.
+    </p>
+  </section>
+
+</main>
   <?php
 }
 
--- plugin_scripts.php
+++ plugin_scripts.php
@@ -1,7 +1,9 @@
 <?php
 function unicon_seo_plugin_scripts() {
     wp_register_style( 'unicon_seo_styles',  plugin_dir_url( __FILE__ ) . 'assets/style.css' );
+    wp_register_script( 'unicon_seo_script',  plugin_dir_url( __FILE__ ) . 'assets/main.js' );
     wp_enqueue_style( 'unicon_seo_styles' );
+    wp_enqueue_script( 'unicon_seo_script' );
 }
 add_action( 'admin_enqueue_scripts', 'unicon_seo_plugin_scripts' );
 ?>
