eb41f8ffa33c924fc51367eec58139590a648cea
Tittu Varghese <tittuhpd@gmail.com>
2017-08-26 23:54:46 +0530

    Minor path fixes.

--- lib/plugin_scripts.php
+++ lib/plugin_scripts.php
@@ -1,7 +1,7 @@
 <?php
 function unicon_seo_plugin_scripts() {
-    wp_register_style( 'unicon_seo_styles',  plugin_dir_url( __FILE__ ) . 'assets/style.css' );
-    wp_register_script( 'unicon_seo_script',  plugin_dir_url( __FILE__ ) . 'assets/main.js' );
+    wp_register_style( 'unicon_seo_styles',  plugin_dir_url( __FILE__ ) . '../assets/style.css' );
+    wp_register_script( 'unicon_seo_script',  plugin_dir_url( __FILE__ ) . '../assets/main.js' );
     wp_enqueue_style( 'unicon_seo_styles' );
     wp_enqueue_script( 'unicon_seo_script' );
     wp_enqueue_media();
